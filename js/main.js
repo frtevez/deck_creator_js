22b2493a528bd5f7c5cb3ce03ed7c43544c4247e
Francisco Tevez <franciscoraultevez@gmail.com>
2024-07-07 02:22:34 -0300

    Base deck creator finished

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// Creador de mazo
+
+
+let userStats = [];
+let cards = [];
+
+class CardStructure {
+    constructor(id, name, description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    };
+
+};
+
+alert('Welcome to the deck creator.')
+alert('You will first be asked to name your custom cards\' stats.\nThen you will be asked to fill in all the individual cards\' details.')
+alert('If you wish to stop creating new stats or cards, do not fill in the next stat name or card name.')
+createDeck()
+
+
+function createDeck() {
+    for (let i = 0; Infinity; i++) {
+
+        if (createStat() == false) {
+            break
+        }
+    }
+
+    for (let i = 0; Infinity; i++) {
+
+        if (createCard(i) == false) {
+            break
+        }
+    }
+    console.log(cards);
+}
+
+function createStat() {
+    let newStat = prompt('Enter a stat name:');
+    if (newStat == '' || newStat == null) {
+        return false;
+    }
+    CardStructure.prototype[newStat] = 'User Stat';
+    userStats.push(newStat);
+    return true;
+};
+
+function createCard(i) {
+    let cardName = prompt('Enter a card\'s name:');
+    if (cardName == '' || cardName == null) {
+        return false;
+    }
+    let cardDescription = prompt('Enter the card\'s description:')
+    let card = new CardStructure(i, cardName, cardDescription);
+    userStats.forEach((stat) => {
+        card[stat] = prompt('Insert this card\'s ' + stat + ' value:')
+    })
+    cards.push(card);
+    return true;
+}
+
